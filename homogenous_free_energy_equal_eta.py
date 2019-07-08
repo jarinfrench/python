482c106bc8b1fa561c9b99b6e6be0bc4753c72f3
Jarin French <jarin.french@inl.gov>
2019-07-08 08:30:21 -0600

    Modified how the initial matrices were created to be more Pythonic. Minor changes.

--- homogenous_free_energy_equal_eta.py
+++ homogenous_free_energy_equal_eta.py
@@ -7,14 +7,11 @@ from matplotlib.pyplot import plot, title, xlim, ylim, xlabel, ylabel, show, leg
 from math import sqrt
 import matplotlib.ticker as ticker
 
-gamma = [0.7, 1, 1.5, 2, 4]
-free_energy = [None] * len(gamma)
-eta_saddle = [None] * len(gamma)
-f0_saddle = [None] * len(gamma)
-etas = []
-
-for i in range(0, 101):
-    etas.append(i / 100)
+gamma = [0.2, 0.5, 0.7, 1, 1.5, 2, 4]
+free_energy = [None for i in range(len(gamma))]
+eta_saddle = [None for i in range(len(gamma))]
+f0_saddle = [None for i in range(len(gamma))]
+etas = [i / 100.0 for i in range(101)]
 
 for i in range(0, len(gamma)):
     en = []
@@ -30,10 +27,10 @@ for i in range(0, len(gamma)):
 plot(eta_saddle,f0_saddle,'kx', label='Saddle point')
 ax = gca()
 ax.set_xlim([0,1])
-ax.set_ylim([0,1.5])
+ax.set_ylim([-0.5,1.5])
 ax.set_xlabel(r'$\eta_i = \eta_j$')
 ax.set_ylabel(r'$f_0(\eta_i = \eta_j)$')
 ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5)) # Changes the major tick marks to be every 0.5
-legend(loc=0, numpoints = 1) # loc = 2 specifies the upper left of the plot, numpoints specifies the number of points shown in the legend
+legend(loc=0, numpoints = 1) # loc = 0 specifies the upper left of the plot, numpoints specifies the number of points shown in the legend
 savefig('free_energy_evolution_equal_eta.eps')
 show()
--- moelans_homogeneous_free_energy.py
+++ moelans_homogeneous_free_energy.py
@@ -7,6 +7,7 @@ from __future__ import division
 from sys import argv
 from matplotlib.pyplot import contourf, show, subplots, figure, axes, xlim, ylim, xlabel, ylabel, colorbar, title, plot, annotate
 from matplotlib.animation import FuncAnimation
+from matplotlib import cm
 from math import sqrt
 from numpy import meshgrid, array, linalg
 
@@ -22,18 +23,10 @@ else:
 
 fig = figure()
 ax = axes(xlim=(0,1), ylim=(0,1))
-gamma = []
-eta_saddle = []
-eta_i = [None] * 100
-eta_j = [None] * 100
-
-for i in range(0, 81):
-    gamma.append(float(i) / 20.0)
-    eta_saddle.append(1.0 / sqrt(2.0 * gamma[i] + 1.0))
-
-for i in range(0, len(eta_i)):
-    eta_i[i] = float(i) / len(eta_i)
-    eta_j[i] = float(i) / len(eta_j)
+gamma = [i / 20.0 for i in range(81)]
+eta_saddle = [1.0 / sqrt(2.0 * gamma[i] + 1.0) for i in range(81)]
+eta_i = [i / 100.0 for i in range(101)]
+eta_j = [i / 100.0 for i in range(101)]
 
 eta_i, eta_j = meshgrid(eta_i, eta_j)
 
@@ -41,9 +34,9 @@ eta_i, eta_j = meshgrid(eta_i, eta_j)
 def animate(k):
     fig.clear() # clear the figure so we don't have a plot that consists of colorbars
     # This is how the free energy is calculated according to Moelans et al.
-    free_energy = array((eta_i**4.0 / 4.0) - (eta_i**2.0 / 2.0) + (eta_j**4.0 / 4.0) - (eta_j**2.0 / 2.0) + (gamma[k] * eta_i**2.0 * eta_j**2.0) + 1.0 / 4.0)
-    free_energy = free_energy / linalg.norm(free_energy) # FIXME: Figure out how to normalize the data.
-    cont = contourf(eta_i, eta_j, free_energy, 40)
+    free_energy = array((eta_i**4.0 / 4.0) - (eta_i**2.0 / 2.0) + (eta_j**4.0 / 4.0) - (eta_j**2.0 / 2.0) + (gamma[k] * eta_i**2.0 * eta_j**2.0) + 0.25)
+    #free_energy = free_energy / linalg.norm(free_energy)
+    cont = contourf(eta_i, eta_j, free_energy, [0.05, 0.1, 0.125, 0.15, 0.2, 0.25], cmap = cm.jet, extend = 'both')
     plot(eta_saddle[k], eta_saddle[k],'wx') # Saddle point plot command - plotted as a white cross ('wx')
     # This next line creates the annotation that identifies where the saddle point is.
     xlabel(r'$\eta_i$')
@@ -53,7 +46,7 @@ def animate(k):
     colorbar() # useful for scaling
     return cont
 
-ani = FuncAnimation(fig, animate, frames = len(gamma), interval=150)
+ani = FuncAnimation(fig, animate, frames = len(gamma), interval=1000)
 if saveMovie:
     ani.save('homogeneous_free_energy_landscape.mp4')
 show()
